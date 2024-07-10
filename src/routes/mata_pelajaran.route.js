c1001d6e74afe397798605100f00793c00f5b193
Faza Alhakim <fazafal04@gmail.com>
2024-07-10 08:34:47 +0700

    verify token mata pelajaran

--- src/routes/mata_pelajaran.route.js
+++ src/routes/mata_pelajaran.route.js
@@ -3,7 +3,8 @@ const express = require("express")
 const router = express.Router()
 
 const {index} = require ('../controllers/mata_pelajaran.controller')
+const {verifyToken} = require ('../middlewares/auth')
 
-router.get("/:id", index)
+router.get("/:id",verifyToken, index)
 
 module.exports = router
