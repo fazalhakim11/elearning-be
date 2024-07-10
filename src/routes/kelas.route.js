adb1444ced48aeaeb7845b613a50c2dd86dec955
Faza Alhakim <fazafal04@gmail.com>
2024-07-10 08:39:18 +0700

    verify token kelas

--- src/routes/kelas.route.js
+++ src/routes/kelas.route.js
@@ -3,7 +3,8 @@ const express = require("express")
 const router = express.Router()
 
 const {index} = require ('../controllers/kelas.cotroller')
+const {verifyToken} = require("../middlewares/auth")
 
-router.get("/", index)
+router.get("/",verifyToken, index)
 
 module.exports = router
